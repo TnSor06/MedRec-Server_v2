7bb423a55d43d613bf8b91e9a61ceeedcfd99823
Tnsor06 <pra006mod@gmail.com>
2020-09-28 17:22:37 +0530

    Fields Searchable

--- src/index.js
+++ src/index.js
@@ -1,28 +1,57 @@
-import {
-    GraphQLServer
-} from 'graphql-yoga'
+import { GraphQLServer } from "graphql-yoga";
+import express from "express";
+import { resolvers, fragmentReplacements } from "./resolvers/index";
 
-import {
-    resolvers,
-    fragmentReplacements
-} from './resolvers/index'
+import prisma from "./prisma";
+import allTypes from "./schema/index";
+const startup_debug = require("debug")("app:startup");
+const url = require("url");
 
-import prisma from './prisma'
-import allTypes from './schema/index'
-const startup_debug = require('debug')('app:startup')
+const port = process.env.PORT || 4000;
 
 const server = new GraphQLServer({
-    typeDefs: allTypes,
-    resolvers,
-    fragmentReplacements,
-    context(request) {
-        return {
-            prisma,
-            request
-        }
-    }
-})
+  typeDefs: allTypes,
+  resolvers,
+  fragmentReplacements,
+  context(request) {
+    return {
+      prisma,
+      request,
+    };
+  },
+});
 
-server.start((server_meta) => {
-    startup_debug('Server is Up\n', server_meta)
-})
+server.express.use(express.static("build"));
+
+server.express.get("/", express.static("build"));
+
+server.express.get("*", (req, res, next) => {
+  // small logic ..
+  const routes = ["/graphql", "/subscriptions", "/playground"];
+
+  if (routes.includes(req.url)) {
+    return next();
+  }
+  // here you can use your way to get the path dir ..
+  res.redirect(
+    url.format({
+      pathname: "/",
+      query: {
+        redirect: req.url,
+      },
+    })
+  );
+});
+
+const options = {
+  port: port,
+  endpoint: "/graphql",
+  subscriptions: "/subscriptions",
+  playground: "/playground",
+};
+
+console.log(process.env.PUBLIC_URL);
+
+server.start(options, (server_meta) => {
+  startup_debug("Server is Up\n", server_meta);
+});
