5bda391f0e6c0c384d0290f56d2f4e142ab885ae
Tnsor06 <pra006mod@gmail.com>
2020-10-01 10:41:53 +0530

    New Changes

---
+++ build/precache-manifest.55c5486f8237cd57b13e14ed64d2c27b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c777e34c7ec2b9c0102d04bb79f72888",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4aa50a4fca46d2e0c6b9",
+    "url": "/static/css/main.c70eda16.chunk.css"
+  },
+  {
+    "revision": "21a1bc12d5ba41b111e3",
+    "url": "/static/js/2.77fcd714.chunk.js"
+  },
+  {
+    "revision": "4aa50a4fca46d2e0c6b9",
+    "url": "/static/js/main.02325343.chunk.js"
+  },
+  {
+    "revision": "1461453640a2cd11dc1d",
+    "url": "/static/js/runtime-main.b52bdbc7.js"
+  }
+]);
