b38c1c1c5787b7b9e290333811c3a0a0e240da1b
Tnsor06 <pra006mod@gmail.com>
2020-10-01 16:05:49 +0530

    FInal

--- src/prisma.js
+++ src/prisma.js
@@ -2,12 +2,10 @@ import { Prisma } from "prisma-binding";
 
 import { fragmentReplacements } from "./resolvers/index";
 
-import config from "config";
-
 const prisma = new Prisma({
   typeDefs: "src/generated/prisma.graphql", // Specify typedefs not datamodel
   endpoint: process.env.PRISMA_ENDPOINT, // where prisma graphql api lives
-  secret: config.get("prisma-secret"),
+  secret: process.env.PRISMA_SECRET,
   fragmentReplacements,
 });
 
--- src/utils/genUserToken.js
+++ src/utils/genUserToken.js
@@ -1,17 +1,20 @@
 // JWT for data transmission for authtoken and authorization
-import jwt from 'jsonwebtoken'
-import config from 'config'
+import jwt from "jsonwebtoken";
 
 const genUserToken = (user) => {
-    const token = jwt.sign({
-        id: user.id,
-        role: user.role,
-        isAdmin: user.isAdmin,
-        verified: user.verified
-    }, config.get('secret-key'), {
-        expiresIn: "3 days"
-    })
-    return token
-}
+  const token = jwt.sign(
+    {
+      id: user.id,
+      role: user.role,
+      isAdmin: user.isAdmin,
+      verified: user.verified,
+    },
+    process.env.SECRET_KEY,
+    {
+      expiresIn: "3 days",
+    }
+  );
+  return token;
+};
 
-export default genUserToken
+export default genUserToken;
