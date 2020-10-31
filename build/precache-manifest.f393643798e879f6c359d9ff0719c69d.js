bb486c156a6b468296eba2ef267f666aa7362011
Tnsor06 <pra006mod@gmail.com>
2020-10-31 06:38:34 +0530

    Playground working on CLient side

--- build/precache-manifest.f393643798e879f6c359d9ff0719c69d.js
+++ build/precache-manifest.f393643798e879f6c359d9ff0719c69d.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4cba6db763857af1398973f350bf61ce",
+    "revision": "0725ccae506b5a0520cb403823fa9a49",
     "url": "/index.html"
   },
   {
-    "revision": "daf2b890507b376cebc4",
+    "revision": "037e08aa8f3183c12fa9",
     "url": "/static/css/main.c70eda16.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.77fcd714.chunk.js"
   },
   {
-    "revision": "daf2b890507b376cebc4",
-    "url": "/static/js/main.b65efc6d.chunk.js"
+    "revision": "037e08aa8f3183c12fa9",
+    "url": "/static/js/main.0c2fdf3b.chunk.js"
   },
   {
     "revision": "1461453640a2cd11dc1d",
